04f3e652a8239360c61e16322a122bd77e7f9390
Udokailodigwe <ilodigweudoka@gmail.com>
2021-09-23 02:12:04 +0300

    Deleted sample code test and created Pokemon list for new Pokedex app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
+//Pokedex application
 
-let favoriteFood = 'peppersoup';
-document.write('peppersoup');
+// pokemonList variable
+let pokemonList = [
+   {name: 'butterfree', height: 6, type: ['bug', 'flying']}, 
+   {name: 'onix', height:7, type: ['rock', 'ground']}, 
+   {name: 'vileplume', height:2, type: ['grass', 'poison']},
+   {name: 'poliwrath', height:3, type: ['water', 'fighting']}
+];
