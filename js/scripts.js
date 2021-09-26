17cf3d1fe537f47868aca7e4a8a10b583dce50cc
Udokailodigwe <ilodigweudoka@gmail.com>
2021-09-26 06:38:15 +0300

    Added iteration using for loop, as well as conditions to highlight an item.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 //Pokedex application
 
-// pokemonList variable
+// PokemonList variable
 let pokemonList = [
-   {name: 'butterfree', height: 6, type: ['bug', 'flying']}, 
-   {name: 'onix', height:7, type: ['rock', 'ground']}, 
-   {name: 'vileplume', height:2, type: ['grass', 'poison']},
-   {name: 'poliwrath', height:3, type: ['water', 'fighting']}
+   {name: 'Butterfree', height: 6, type: ['Bug', 'Flying']}, 
+   {name: 'Onix', height:7, type: ['Rock', 'Ground']}, 
+   {name: 'Vileplume', height:2, type: ['Grass', 'Poison']},
+   {name: 'Poliwrath', height:3, type: ['Water', 'Fighting']}
 ];
+
+// Printed the names and hieght of the element by iterating the pokemon list
+for (let i = 0; i < pokemonList.length; i++){
+document.write(pokemonList[i].name +' ' + 'height:' + pokemonList[i].height +'.');
+
+// Highlighted individual pokemon item
+if (pokemonList[i].height > 6 && pokemonList[i].height < 10){
+document.write(' - Wow, that\'s big! ');
+}else document.write('  ')
+}
+
+
+
+
+
+
