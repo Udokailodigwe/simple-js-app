c7d97883e9e9235aab56e7741493368a4ff30a91
Udokailodigwe <ilodigweudoka@gmail.com>
2021-10-04 01:01:03 +0300

    Added loop function for pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 //Pokedex application
 
 // PokemonList variable
-var pokemonRepository = (function(){
+let pokemonRepository = (function(){
 
    // Added to pokemonList array 
-   var pokemonList =  [ 
+   let pokemonList =  [ 
       {name: 'Butterfree', height: 6, type: ['Bug', 'Flying']},
       {name: 'Onix', height:7, type: ['Rock', 'Ground']}, 
       {name: 'Vileplume', height:2, type: ['Grass', 'Poison']},
       {name: 'Poliwrath', height:3, type: ['Water', 'Fighting']} 
    ];
 
-   // Allows for the increament of items in pokemonList array
-   function add(Pokemon){
+   // Allows for the increment of items in pokemonList array
+   function add(pokemon){
       if(typeof pokemon === 'object' && 'name' in pokemon) //Checking if items match required before adding to pokemonList array
-      { pokemonList.push(Pokemon); //Adds pokemon to the pokemonList array
+      { pokemonList.push(pokemon); //Adds pokemon to the pokemonList array
       }else {
          console.log('please search for the right pokemon')
       }
@@ -23,7 +23,7 @@ var pokemonRepository = (function(){
    function getAll (){
       return pokemonList;
    }
-
+   //Returned all defined function keys
    return {
       add : add,
       getAll : getAll
@@ -33,29 +33,7 @@ var pokemonRepository = (function(){
 
 //Loops through pokemoList array
 pokemonRepository.getAll().forEach(function(pokemon){
-   document.write(pokemon + '<br>');
+   document.write(pokemon.name + '  ' + '(' + 'height:' + ' ' + pokemon.height + ')' + ' ' + pokemon.type + '<br>'); //Displays all pokemonList items in the browser
 });
 
 
-
-
-
-
-// Printed the names and hieght of the element by iterating the pokemon list.
-
-//    let pokeName = pokemonList.name;
-//    let pokeHeight = pokemonList.height;
-// // for (let i = 0; i < pokemonList.length; i++)
-// pokemonList.forEach(function(pokemonList){
-   
-// });
-
-   
-// if (pokeHeight > 5 && pokeHeight < 7){
-//    document.write(pokeName + '  ' + '(' + 'height:' + ' ' + pokeHeight + ')' + '<br>' );
-//    }else if (pokeHeight > 6){
-//    document.write(pokeName + '  ' + '(' + 'height:' + ' ' + pokeHeight + ')' + ' - Wow, that\'s a big sized Pokemon!' + '<br>'); //Highlighted the biggest pokemon element in the list.
-//    }else {
-//    document.write(pokeName + '  ' + '(' + 'height:' + ' ' + pokeHeight + ')' + '<br>' );
-//    }
-// }
